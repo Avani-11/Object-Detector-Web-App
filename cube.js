f7731cc905ac04aa25b859101511025e6a4c24ab
Avani-11 <87705294+Avani-11@users.noreply.github.com>
2023-09-27 14:39:57 +0530

    Add files via upload

---
+++ cube.js
@@ -0,0 +1,30 @@
+status1 = "";
+
+function preload() 
+{
+    img = loadImage("odwa-4.jpg");
+}
+
+function setup()
+{
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status1").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!")
+    status1 = true;
+}
+
+function gotResult(error, results)
+{
+    if (error)
+    {
+      console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
